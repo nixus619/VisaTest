f30958e1e0a82e90b7e84aaf2aea1f5ab381cae4
nixus619 <nickdcedor@gmail.com>
2018-02-14 10:23:35 -0500

    Test

--- src/test/java/stepDefinition/annotation.java
+++ src/test/java/stepDefinition/annotation.java
@@ -7,10 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import org.openqa.selenium.By; 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 import cucumber.api.java.*;
 import cucumber.api.java.en.*; 
 
