15ad5bd45f0db06af1d2320b4e06f82747e97363
nixus619 <nickdcedor@gmail.com>
2018-02-14 15:41:31 -0500

    run2

--- src/main/java/code/Sql.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Program Files\MySQL\MySQL Server 5.7\bin
-mysql -u root -p98Custom! dw < C:/Users/Admin/Desktop/insert.txt
--- mysql -u root -p98Custom! dw < C:/Users/Admin/Desktop/delete.txt
-exit
